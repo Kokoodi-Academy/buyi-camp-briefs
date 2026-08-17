eb85c27c6c641938e2727303acb0f894abe954bf
Buyi07 <Damilolaoladejo81@gmail.com>
2026-08-17 14:20:05 +0000

    Added javascript to code

---
+++ Week-3/session1/test/script.js
@@ -0,0 +1,8 @@
+const title = document.getElementById("title");
+const button = document.getElementById("click");
+
+button.addEventListener("click", function(){
+    title.textContent = "Welcome to my website";
+ 
+})   
+
