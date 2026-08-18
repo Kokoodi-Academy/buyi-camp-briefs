fd3026f3a2a7248a10d9d11a03a75290e0afd4bc
Buyi07 <Damilolaoladejo81@gmail.com>
2026-08-18 13:06:23 +0000

    added new css.html and js files to my homwork folder about me

---
+++ HWprofile card/HWprofilecard.js
@@ -0,0 +1,5 @@
+<script>
+  document.getElementById("learnMoreBtn").addEventListener("click", function () {
+    document.getElementById("info").textContent = "Hi! I'm Buyi, and this is my website. I am a learning web developer who understands how to use HTML,CSS and JavaScript"
+  });
+</script>
